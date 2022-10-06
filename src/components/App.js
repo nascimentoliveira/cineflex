7f5b18aaf1e54bb6bfc00ae9bcb84b3aba38cc99
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-10-05 22:09:36 -0300

    feat (App): style reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
+import GlobalStyle from './theme/globalStyle'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/movie/:idMovie' element={<Movie />} />
-        <Route path='/section/:idSection' element={<Section />} />
-        <Route path='/success' element={<Success />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/movie/:idMovie' element={<Movie />} />
+          <Route path='/section/:idSection' element={<Section />} />
+          <Route path='/success' element={<Success />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
